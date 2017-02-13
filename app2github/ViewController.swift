bdb767bbf1ea9723bc085996f21167a5248e651a
sophia1215 <code.sophiawang@gmail.com>
2017-02-14 04:36:14 +0800

    一秒落淚情詩上傳專案到GitHub，錄製 App 操作影片和轉成 gif，在 Medium 貼上 gif 和 專案的 GitHub 連結

--- app2github/ViewController.swift
+++ app2github/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Thread.sleep(forTimeInterval: 2.0) //延長3秒
     }
 
     override func didReceiveMemoryWarning() {
